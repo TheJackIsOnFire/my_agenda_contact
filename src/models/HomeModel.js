0ed1e3f86950460db66121b96bbbce95855d98a1
TheJackIsOnFire <alexsanderonfire@gmail.com>
2025-04-29 17:41:13 -0300

    add eslint e prettier

--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 //Criando schema
 const HomeSchema = new mongoose.Schema({
-  titulo: { type: String, require: true },
-  descricao: String,
+	titulo: { type: String, require: true },
+	descricao: String,
 });
 
 //Criando model
-const HomeModel = mongoose.model("Home", HomeSchema);
+const HomeModel = mongoose.model('Home', HomeSchema);
 
 class Home {}
 
