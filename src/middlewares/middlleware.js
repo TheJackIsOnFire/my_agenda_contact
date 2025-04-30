a1e5682824b3e9ddde241edc7f5a73fcf00315cb
TheJackIsOnFire <alexsanderonfire@gmail.com>
2025-04-30 16:00:19 -0300

    feat: Adicionando criação de usuário

--- src/middlewares/middlleware.js
+++ src/middlewares/middlleware.js
@@ -10,3 +10,9 @@ exports.csrfMiddleware = (req, res, next) => {
 	res.locals.csrfToken = req.csrfToken();
 	next();
 };
+
+exports.middlewareMessages = (req, res, next) => {
+	res.locals.msgErrors = req.flash('messagesErrors');
+	res.locals.msgSuccess = req.flash('messagesSuccess');
+	next();
+};
